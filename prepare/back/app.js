edc655d944e394ecdf34a84adba0af022eebb7cb
ksmself <53217988+ksmself@users.noreply.github.com>
2021-07-19 18:58:14 +0900

    fix: cors

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -34,21 +34,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
   app.use(hpp());
   app.use(helmet());
+  app.use(
+    cors({
+      origin: 'http://chacom.site',
+      credentials: true, // 쿠키 전달을 위해
+    })
+  );
 } else {
   app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true, // 쿠키 전달을 위해
+    })
+  );
 }
-app.use(
-  cors({
-    // origin: https://cha.com, 으로 설정해두면 이 사이트에서 온 요청만 허용하겠다
-    origin: ['http://localhost:3060', 'chacom.site'],
-    credentials: true, // 쿠키 전달을 위해
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      domain: process.env.NODE_ENV === 'production' && '.chacom.site',
-    },
-  })
-);
 
 app.use(express.json()); // 프론트에서 json 형식으로 데이터를 보냈을 때 json 형식의 데이터를 req body안에 넣어주고
 app.use(express.urlencoded({ extended: true })); // 폼 submit을 했을 때 urlencoded 방식으로 넘어오기 때문에
